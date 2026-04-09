43593b9e30cf6e8d6d4e090c8859bad9336503a4
ben-phattharaphon <phattharaphon.spk@gmail.com>
2026-04-09 14:50:37 +0700

    update schema

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(cors({
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
     origin: ["http://localhost:5173"],
-    methods: ["GET","POST","PUT","PATCH","DELETE"],
-    credentials: true
-}))
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
 
-export default app
+export default app;
