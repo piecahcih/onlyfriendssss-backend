792fe29c69d2e5a6c6b8f114a370a9a2ad867635
Herapjs <Polaroidploy2538@gmail.com>
2026-04-10 15:01:09 +0700

    wwt

--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { deleteMe, getMe, updateMe } from '../controllers/account.controller.js'
+import { authCheckUser } from '../middlewares/authenticate.js'
 
 
 const accountRoute = express.Router()
 
-accountRoute.get('/api/account/profile',getMe)
-accountRoute.patch('/api/account/profile', upload.single('profileImg'),updateMe)
-accountRoute.delete('/api/account/profile',deleteMe)
+accountRoute.get('/api/account/profile',authCheckUser ,getMe)
+accountRoute.patch('/api/account/profile',authCheckUser, upload.single('profileImg'),updateMe)
+accountRoute.delete('/api/account/profile',authCheckUser,deleteMe)
 
 export default accountRoute
