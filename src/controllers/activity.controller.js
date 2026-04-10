d37cb1fb24780ab5e519f60f202cdc759195c8aa
piecahcih <pichayapa.thai@gmail.com>
2026-04-10 14:00:34 +0700

    activityctrlsv

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -1,3 +1,6 @@
+import { getAllActivities } from "../services/activity.service.js";
+
 export async function getAllActivitiesCtrl (req,res,next) {
-    
+    const foundActivity = await getAllActivities()
+    res.json({ activities: foundActivity })
 }
--- src/routes/activity.route.js
+++ src/routes/activity.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { getAllActivitiesCtrl } from '../controllers/activity.controller.js'
 
 const activityRoute = express.Router()
 
-activityRoute.get('/',(req,res)=>{res.json("getactivity")})
-activityRoute.post('/',(req,res)=>{res.json("getactivity")})
+activityRoute.get('/',getAllActivitiesCtrl)
+activityRoute.post('/',(req,res)=>{res.json("addactivity")})
 activityRoute.get('/:id',(req,res)=>{res.json("getactivitybyid")})
-activityRoute.put('/:id',(req,res)=>{res.json("getactivitybyid")})
+activityRoute.put('/:id',(req,res)=>{res.json("editactivitybyid")})
 activityRoute.patch('/:id',(req,res)=>{res.json("changeactivitystatus")})
 
 export default activityRoute
