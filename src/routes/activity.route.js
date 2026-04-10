40afeb75ac26cec12b3310c2d4c8e3d180920527
piecahcih <pichayapa.thai@gmail.com>
2026-04-10 17:35:34 +0700

    seeddatajaaa

--- src/routes/activity.route.js
+++ src/routes/activity.route.js
@@ -6,7 +6,7 @@ const activityRoute = express.Router()
 
 activityRoute.get('/',getAllActivitiesCtrl)
 activityRoute.post('/',authCheckUser ,createActivitiesCtrl)
-activityRoute.get('/:id',(req,res)=>{res.json("getactivitybyid")})
+activityRoute.get('/:id',(req,res)=>{res.json("getactivitybyidjaaa")})
 activityRoute.put('/:id',(req,res)=>{res.json("editactivitybyid")})
 activityRoute.patch('/:id',(req,res)=>{res.json("changeactivitystatus")})
 
