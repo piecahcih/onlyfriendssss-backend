5f63890797007cfc1c5601009affd4926b53c146
piecahcih <pichayapa.thai@gmail.com>
2026-04-10 16:55:49 +0700

    seeddata

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,8 @@
-import express from "express";
-import { registerOrLogin } from "../controllers/auth.controller.js";
 import express from 'express'
 import { login, register } from '../controllers/auth.controller.js'
 import {registerOrLogin} from '../controllers/auth.controller.js'
 
-const router = express.Router();
+const authRoute = express.Router();
 
 authRoute.post('/registerOrLogin', registerOrLogin)
 
