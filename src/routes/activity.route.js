bbc6b8f749fad4b981343fd9f171d2b06eafb15b
piecahcih <pichayapa.thai@gmail.com>
2026-04-14 03:30:19 +0700

    getActByCat

--- src/routes/activity.route.js
+++ src/routes/activity.route.js
@@ -1,13 +1,15 @@
 import express from 'express'
-import { changeActivityStatusCtrl, createActivitiesCtrl, deleteActivityByIdCtrl, editActivityByIdCtrl, getActivityByIdCtrl, getAllActivitiesCtrl } from '../controllers/activity.controller.js'
+import { changeActivityStatusCtrl, createActivitiesCtrl, deleteActivityByIdCtrl, editActivityByIdCtrl, getActivityByCategoryCtrl, getActivityByIdCtrl, getAllActivitiesCreatedByThisAccountCtrl, getAllActivitiesCtrl } from '../controllers/activity.controller.js'
 import { authCheckUser } from '../middlewares/authenticate.js'
 
 const activityRoute = express.Router()
 
 activityRoute.get('/',getAllActivitiesCtrl)
-activityRoute.post('/',authCheckUser ,createActivitiesCtrl)
+activityRoute.get('/my-activities',authCheckUser ,getAllActivitiesCreatedByThisAccountCtrl)
 activityRoute.get('/:activityid',getActivityByIdCtrl)
-activityRoute.put('/:activityid',editActivityByIdCtrl)
+activityRoute.get('/category/:category',getActivityByCategoryCtrl)
+activityRoute.post('/',authCheckUser ,createActivitiesCtrl)
+activityRoute.put('/:activityid',authCheckUser ,editActivityByIdCtrl)
 activityRoute.patch('/:activityid',changeActivityStatusCtrl)
 activityRoute.delete('/:activityid',authCheckUser ,deleteActivityByIdCtrl)
 
