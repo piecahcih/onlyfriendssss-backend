52e7673dc0085a2c8132de07167baa6a72516eac
Herapjs <Polaroidploy2538@gmail.com>
2026-04-26 15:19:57 +0700

    pro

--- src/controllers/interest.controller.js
+++ src/controllers/interest.controller.js
@@ -1,6 +1,51 @@
 import createHttpError from 'http-errors'
 import { getUserInterest, getUserSuggestedActivitiesByInterest } from '../services/interest.service.js'
 
+export const INTEREST_MAP = {
+// Food & Drink
+foodie: 'FOOD',
+cafe_hopping: 'FOOD',
+street_food: 'FOOD',
+fine_dining: 'FOOD',
+cooking_baking: 'FOOD',
+drinks_nightout: 'FOOD',
+
+// Health & Active
+slowlife: 'HEALTH',
+health: 'HEALTH',
+sport: 'HEALTH',
+camping: 'HEALTH',
+gym_workout: 'HEALTH',
+yoga_pilates: 'HEALTH',
+running: 'HEALTH',
+mental_wellness: 'HEALTH',
+team_sports: 'HEALTH',
+
+// Art & Culture
+art: 'ART',
+museum_gallery: 'ART',
+photography: 'ART',
+crafting_diy: 'ART',
+live_music: 'ART',
+book_club: 'ART',
+
+// Entertainment & Fun
+gaming: 'ENTERTAINMENT',
+movies_cinema: 'ENTERTAINMENT',
+board_games: 'ENTERTAINMENT',
+video_games: 'ENTERTAINMENT',
+karaoke: 'ENTERTAINMENT',
+concerts_festivals: 'ENTERTAINMENT',
+
+// Travel & Adventure
+travel: 'TRAVEL',
+volunteer: 'TRAVEL',
+backpacking: 'TRAVEL',
+road_trip: 'TRAVEL',
+beach_vibes: 'TRAVEL',
+hiking_trekking: 'TRAVEL',
+sightseeing: 'TRAVEL',
+};
 export async function getUserInterestCtrl(req, res, next) {
     const {id} = req.result
 
@@ -24,54 +69,9 @@ export async function suggestActivityByYourInterestCtrl(req, res, next) {
     if (!userInterests) {
         return next(createHttpError[404]('User has no interest'))
     }
-    console.log('userInterests', userInterests)
+    // console.log('userInterests', userInterests)
 
 
-    export const INTEREST_MAP = {
-    // Food & Drink
-    foodie: 'FOOD',
-    cafe_hopping: 'FOOD',
-    street_food: 'FOOD',
-    fine_dining: 'FOOD',
-    cooking_baking: 'FOOD',
-    drinks_nightout: 'FOOD',
-
-    // Health & Active
-    slowlife: 'HEALTH',
-    health: 'HEALTH',
-    sport: 'HEALTH',
-    camping: 'HEALTH',
-    gym_workout: 'HEALTH',
-    yoga_pilates: 'HEALTH',
-    running: 'HEALTH',
-    mental_wellness: 'HEALTH',
-    team_sports: 'HEALTH',
-
-    // Art & Culture
-    art: 'ART',
-    museum_gallery: 'ART',
-    photography: 'ART',
-    crafting_diy: 'ART',
-    live_music: 'ART',
-    book_club: 'ART',
-
-    // Entertainment & Fun
-    gaming: 'ENTERTAINMENT',
-    movies_cinema: 'ENTERTAINMENT',
-    board_games: 'ENTERTAINMENT',
-    video_games: 'ENTERTAINMENT',
-    karaoke: 'ENTERTAINMENT',
-    concerts_festivals: 'ENTERTAINMENT',
-
-    // Travel & Adventure
-    travel: 'TRAVEL',
-    volunteer: 'TRAVEL',
-    backpacking: 'TRAVEL',
-    road_trip: 'TRAVEL',
-    beach_vibes: 'TRAVEL',
-    hiking_trekking: 'TRAVEL',
-    sightseeing: 'TRAVEL',
-    };
 
     
     const userInterestCategories = []
