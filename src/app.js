df5b1da81a82b7caf81a252a8ad4ba632dc9b9bb
piecahcih <pichayapa.thai@gmail.com>
2026-05-14 00:29:41 +0700

    tried to deploy

--- src/app.js
+++ src/app.js
@@ -19,16 +19,36 @@ import placeRoute from "./routes/place.route.js";
 const app = express();
 const server = createServer(app)
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://stick-silicon-premiere-hamburg.trycloudflare.com"
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  }),
-);
+const allowedOrigins = [
+  'https://onlyfriendssss.vercel.app', // Your production frontend
+  'http://localhost:5173',             // Your local development (Vite default)
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true // Crucial if you plan to use cookies or sessions later
+}));
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       'https://onlyfriendssss.vercel.app',
+//       "https://stick-silicon-premiere-hamburg.trycloudflare.com"
+//     ],
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     credentials: true,
+//   }),
+// );
 app.use('/uploads', express.static('public/uploads'));
 
 app.use("/api/auth", authRoute);
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,13 +2,23 @@ import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
+// const adapter = new PrismaMariaDb({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+//   connectionLimit: 5,
+// });
+const databaseUrl = new URL(process.env.DATABASE_URL);
+
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
-});
+    host: databaseUrl.hostname,
+    port: parseInt(databaseUrl.port) || 3306,
+    user: decodeURIComponent(databaseUrl.username),
+    password: decodeURIComponent(databaseUrl.password),
+    database: databaseUrl.pathname.slice(1),
+    connectionLimit: 5
+})
 
 const prisma = new PrismaClient({ adapter });
 
