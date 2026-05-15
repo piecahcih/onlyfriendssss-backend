255f41952048ba38d4acc2a98cbef6fdd0e98186
piecahcih <pichayapa.thai@gmail.com>
2026-05-15 13:11:21 +0700

    folderuploadss

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -5,6 +5,7 @@ import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const uploadDir = path.join(__dirname, "../uploads");
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
@@ -12,7 +13,7 @@ if (!fs.existsSync(uploadDir)) {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads")); //ชื่อโฟลเดอร์ที่สร้างไว้
+    cb(null, uploadDir); //ชื่อโฟลเดอร์ที่สร้างไว้
   },
   filename: (req, file, cb) => {
     // ตั้งชื่อไฟล์: วันที่ปัจจุบัน + นามสกุลไฟล์เดิม
