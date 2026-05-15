42d516d53b0e7f46a844932f0657151f9f4c454b
piecahcih <pichayapa.thai@gmail.com>
2026-05-15 12:22:35 +0700

    mysqldtbfirebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,7 +2,13 @@ import admin from 'firebase-admin'
 import {createRequire} from 'module'
 
 const require = createRequire(import.meta.url)
-const serviceAccount = require('../../serviceAccountKey.json')
+let serviceAccount;
+
+if(process.env.FIREBASE_SERVICE_ACCOUNT){
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('../../serviceAccountKey.json')
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
